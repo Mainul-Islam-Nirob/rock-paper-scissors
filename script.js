0d56a35330152feb1d54c0aa733e5fd763b4b532
Mainul Islam <maainul.islam@gmail.com>
2022-02-15 20:59:19 +0600

    Basic functionality and Basic Design complete, now need to improve design and optimize js code

--- script.js
+++ script.js
@@ -1,58 +1,103 @@
-function userPlay() {
-    let userInput = prompt("Rock, Paper or Scissors?");
-    let lowerCaseInput = userInput.toLowerCase();
-
-    if (lowerCaseInput == "rock" || lowerCaseInput == "paper" || lowerCaseInput == "scissors") {
-        return lowerCaseInput;
-    }else{
-        return false;
-    }
-
-}
-
 function computerPlay() {
     const option = ['rock', 'paper', 'scissors'];
     const random = Math.floor(Math.random() * option.length)
     return option[random];
 }
 
+
 function playRound(ps, cs) {
-    
+
     if(ps == cs){
-        return "Tie"
+        return ["Tie! you both are same", 'tie']
+
     }else if(ps == "rock" && cs == "scissors"){
-        return "You Win! Rock beats Scissors";
+        let msg = "You Win! Rock beats Scissors";
+        
+
+        return [msg, "ps"];
+
     }else if(ps == "scissors" && cs == "rock") {
-        return "You Lose! Rock beats Scissors";
+        let msg = "You Lose! Scissors beats Paper";
+        
+        return [msg, "cs"];
 
     }else if(ps == "paper" && cs == "rock"){
-        return "You Win! Paper beats Rock";
+        let msg = "You Win! Rock beats Scissors";
+        
+        return [msg, "ps"];
 
     } else if(ps == "rock" && cs == "paper"){
-        return "You Lose! Paper beats Rock";
+        let msg = "You Lose! Scissors beats Paper";
+        
+        return [msg, "cs"];
 
     } else if (ps == "paper" && cs == "scissors") {
-        return "You Lose! Scissors beats Paper";
+        let msg = "You Lose! Scissors beats Paper";
+        
+        return [msg, "cs"];
 
     } else if (ps == "scissors" && cs == "paper"){
-        return "You Win! Scissors beats Paper";
+        let msg = "You Win! Rock beats Scissors";
+        
+        return [msg, "ps"];
 
     }
 }
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = userPlay();
-        const computerSelection = computerPlay();
 
-        if (playerSelection) {
-            console.log(playRound(playerSelection, computerSelection));
-        } else {
-            console.log("Your input should be either 'rock', 'paper' or 'scissors'");
+let buttons = document.querySelectorAll("button");
+let resultContainer = document.getElementById("result");
+let cScore = document.getElementById("cScore");
+let pScore = document.getElementById("pScore");
+let winnerContainer = document.getElementById("winner");
+
+
+
+buttons.forEach(button => {
+    
+    button.addEventListener("click", (e) => {
+
+        let playerSelection = e.target.innerText.toLowerCase();
+
+        let result = playRound(playerSelection, computerPlay());
+        
+        // console.log(result);
+
+        let paragraph = document.createElement("p").innerText = result[0];
+       resultContainer.innerHTML = paragraph;
+
+
+        if (result[1] == "ps") {
+            let pScoreNum = ~~pScore.innerText;
+
+            if (pScoreNum >= 4) {
+               
+                resultContainer.innerText = "";
+                winnerContainer.innerText = "You are the winner! You: 5 & Computer: " + " " + cScore.innerText;
+                pScore.innerText = 0;
+                cScore.innerText = 0;
+            }else{
+                pScore.innerText = pScoreNum + 1 ;
+                winnerContainer.innerText = ""
+
+            } 
+
+        }else if(result[1] == "cs"){
+            let cScoreNum = ~~cScore.innerText;
+            if (cScoreNum >= 4) {
+            
+                resultContainer.innerText = "";
+                winnerContainer.innerText = "Computer is the winner! Computer: 5 & You:" + " " + pScore.innerText;
+
+                cScore.innerText = 0;
+                pScore.innerText = 0;
+
+            } else {
+                cScore.innerText = cScoreNum + 1;
+                winnerContainer.innerText = ""
+            } 
         }
+        
 
-    }
-}
-
-
-game()
+    })
+})
